762d4621f45f44fa75f2b015055efd5574eee627
Александр <Alexander.popov.usurt@mail.ru>
2023-05-08 19:17:36 +0500

    Урок Spring и Mockito

---
+++ src/main/java/com/example/lessonmockito/LessonMockitoApplication.java
@@ -0,0 +1,13 @@
+package com.example.lessonmockito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LessonMockitoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LessonMockitoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lessonmockito/LessonMockitoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lessonmockito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LessonMockitoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
